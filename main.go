e3c68e2480e260db7807a97085e6e30ea1cdacd5
tqtwin <thangtran081003@gmail.com>
2024-11-09 13:59:40 +0700

    Updated code with MongoDB integration

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	socketio "github.com/googollee/go-socket.io"
@@ -10,13 +12,46 @@ import (
 	"github.com/googollee/go-socket.io/engineio/transport"
 	"github.com/googollee/go-socket.io/engineio/transport/polling"
 	"github.com/googollee/go-socket.io/engineio/transport/websocket"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Global variable for the socket server
+// Global variables for the socket server and MongoDB client
 var socketServer *socketio.Server
+var mongoClient *mongo.Client
+var messagesCollection *mongo.Collection
 
 var allowOriginFunc = func(r *http.Request) bool {
 	return true
+}
+
+// Message struct represents a chat message
+type Message struct {
+	ID      string    `bson:"_id,omitempty"`
+	Sender  string    `bson:"sender"`
+	Content string    `bson:"content"`
+	Time    time.Time `bson:"time"`
+}
+
+// MongoDB connection function
+func connectToMongoDB() {
+	var err error
+	// Replace with your MongoDB URI
+	clientOptions := options.Client().ApplyURI("mongodb+srv://admin:admin@admin.qboa2og.mongodb.net/socket")
+	mongoClient, err = mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal("MongoDB connection error:", err)
+	}
+
+	// Verify connection
+	err = mongoClient.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal("MongoDB ping error:", err)
+	}
+
+	// Set collection for messages
+	messagesCollection = mongoClient.Database("socket").Collection("messages")
+	log.Println("Connected to MongoDB!")
 }
 
 // Exported function that will be called by Vercel
@@ -26,6 +61,9 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Connect to MongoDB
+	connectToMongoDB()
+
 	// Initialize the Socket.IO server
 	socketServer = socketio.NewServer(&engineio.Options{
 		Transports: []transport.Transport{
@@ -50,6 +88,20 @@ func main() {
 	socketServer.OnEvent("/", "message", func(s socketio.Conn, msg string) {
 		log.Println("Message received:", msg)
 		socketServer.BroadcastToRoom("/", "chat", "message", msg)
+
+		// Save the message to MongoDB
+		message := Message{
+			Sender:  s.ID(), // Store sender ID as an example
+			Content: msg,
+			Time:    time.Now(),
+		}
+
+		_, err := messagesCollection.InsertOne(context.TODO(), message)
+		if err != nil {
+			log.Println("Failed to save message:", err)
+		} else {
+			log.Println("Message saved to MongoDB")
+		}
 	})
 
 	// Handle disconnection
